36a936c94a72a72e4fe8457e4f655933a280253d
Juan <juan.nascimento.999@gmail.com>
2022-10-30 23:14:28 -0300

    bugfix version java

--- projeto-xadrez/src/application/Program.java
+++ projeto-xadrez/src/application/Program.java
@@ -17,29 +17,29 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch,captured);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.println("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				if(chessMatch.getPromoted() != null) {
+				if (chessMatch.getPromoted() != null) {
 					System.out.println("Enter piece for (B/N/R/Q): ");
 					String type = sc.nextLine().toUpperCase();
 					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
@@ -47,14 +47,13 @@ public class Program {
 						type = sc.nextLine().toUpperCase();
 					}
 					chessMatch.replacePromotedPiece(type);
-					
+
 				}
-				
+
 			} catch (ChessException e) {
 				System.out.println(e.getLocalizedMessage());
 				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
